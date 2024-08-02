2d2f294adff5639bb261be89dc5a6b95ad9ae332
Sacha Wharton <32266273+sachajw@users.noreply.github.com>
2024-08-02 12:17:57 +0200

    🛠 NEW: netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import cloudflare from "@astrojs/cloudflare";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare()
+  adapter: netlify()
 });
